e7389b180f074f4e330d3a981b67ea4170bf8fd5
ilyas13z <ilyas13z@yandex.ru>
2025-07-30 17:32:38 +0500

    did the first and second task

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,50 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Task(BaseModel):
+    name: str
+    status: str
+
+tasks_dict = dict()
+
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks_dict
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(task: str):
+    task = task.split()
+    if len(tasks_dict) == 0:
+        tasks_dict[0] = [task[0], task[1]]
+        task_id = 0
+    else:
+        task_id = list(tasks_dict.keys())[-1] + 1
+        tasks_dict[task_id] = [task[0], task[1]]
+    return {"Task id": task_id, "Task name": task[0], "Task status": task[1]}
+
+    
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, task: Task):
+    try:
+        if task_id in tasks_dict.keys():
+            tasks_dict[task_id] = [task.name, task.status]
+            return {"Task name": task.name, "Task status": task.status}
+        else:
+            raise KeyError
+    except KeyError:
+        return {"KeyError": "task_id not found"}
+    except:
+        return {"Updating": "Deleting went wrong"}
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    try:
+        temp = tasks_dict[task_id]
+        del tasks_dict[task_id]
+        return {"Task id": task_id, "Task name": temp[0], "Task status": temp[1]}
+    except:
+        return {"Error": "Deleting went wrong"}
