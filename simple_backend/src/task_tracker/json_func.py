94b0cfd797cd63e6b9942eb0e0b1392d6d3dc52e
ilyas13z <ilyas13z@yandex.ru>
2025-07-30 20:30:08 +0500

    Change func with json file in class object

--- simple_backend/src/task_tracker/json_func.py
+++ simple_backend/src/task_tracker/json_func.py
@@ -1,13 +1,16 @@
 import json
 
 
-def read_tasks():
-    with open("tasks.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-        data = {int(i): data[i] for i in data.keys()}
-    return data
+class JsonFileWorker:
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def read_file(self):
+        with open(self.file_name, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            data = {int(i): data[i] for i in data.keys()}
+        return data
 
-
-def write_tasks(data):
-    with open("tasks.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+    def write_file(self, data):
+        with open(self.file_name, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,9 +1,11 @@
-from json_func import read_tasks, write_tasks
+from json_func import JsonFileWorker
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+file_worker = JsonFileWorker("tasks.json")
 
 class Task(BaseModel):
     name: str
@@ -12,13 +14,13 @@ class Task(BaseModel):
 
 @app.get("/tasks")
 def get_tasks():
-    tasks_dict = read_tasks()
+    tasks_dict = file_worker.read_file()
     return tasks_dict
 
 @app.post("/tasks")
 def create_task(task: str):
     task = task.split()
-    tasks_dict = read_tasks()
+    tasks_dict = file_worker.read_file()
 
     if len(tasks_dict) == 0:
         tasks_dict[0] = [task[0], task[1]]
@@ -26,7 +28,7 @@ def create_task(task: str):
     else:
         task_id = list(tasks_dict.keys())[-1] + 1
         tasks_dict[task_id] = [task[0], task[1]]
-    write_tasks(tasks_dict)
+    file_worker.write_file(tasks_dict)
     return {"Task id": task_id, "Task name": task[0], "Task status": task[1]}
 
     
@@ -34,10 +36,10 @@ def create_task(task: str):
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, task: Task):
     try:
-        tasks_dict = read_tasks()
+        tasks_dict = file_worker.read_file()
         if task_id in tasks_dict.keys():
             tasks_dict[task_id] = [task.name, task.status]
-            write_tasks(tasks_dict)
+            file_worker.write_file(tasks_dict)
             return {"Task name": task.name, "Task status": task.status}
         else:
             raise KeyError
@@ -49,10 +51,10 @@ def update_task(task_id: int, task: Task):
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
     try:
-        tasks_dict = read_tasks()
+        tasks_dict = file_worker.read_file()
         temp = tasks_dict[task_id]
         del tasks_dict[task_id]
-        write_tasks(tasks_dict)
+        file_worker.write_file(tasks_dict)
         return {"Task id": task_id, "Task name": temp[0], "Task status": temp[1]}
     except:
         return {"Error": "Deleting went wrong"}
