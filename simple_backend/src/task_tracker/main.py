79befeff168981f5b53b31c6897579ec90ab29eb
ilyas13z <ilyas13z@yandex.ru>
2025-08-05 00:15:23 +0500

    Add abstract class and make inheritance from base class in clients

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,10 +1,7 @@
 import os
 
-from json_func import JsonBinWorker
-from llm_func import LlmWorker
+from abstract import AiClient, JsonBinClient
 from dotenv import load_dotenv
-
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -12,9 +9,13 @@ load_dotenv()
 
 app = FastAPI()
 
-# file_worker = JsonFileWorker("tasks.json")
-file_worker = JsonBinWorker(os.getenv("API_KEY_JSONBIN"))
-description_worker = LlmWorker(os.getenv("API_KEY_LLM"), os.getenv("USER_ID_LLM"))
+file_worker = JsonBinClient(
+    os.getenv("API_KEY_JSONBIN"), os.getenv("USER_ID_JSONBIN")
+)
+description_worker = AiClient(
+    os.getenv("API_KEY_LLM"), os.getenv("USER_ID_LLM")
+)
+
 
 class Task(BaseModel):
     name: str
@@ -26,6 +27,7 @@ class Task(BaseModel):
 def get_tasks():
     tasks_dict = file_worker.read()
     return tasks_dict
+
 
 @app.post("/tasks")
 def create_task(name: str, status: str):
@@ -39,9 +41,13 @@ def create_task(name: str, status: str):
         task_id = list(tasks_dict.keys())[-1] + 1
         tasks_dict[task_id] = [name, status, description]
     file_worker.write(tasks_dict)
-    return {"Task id": task_id, "Task name": name, "Status": status, "Description": description}
+    return {
+        "Task id": task_id,
+        "Task name": name,
+        "Status": status,
+        "Description": description,
+    }
 
-    
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, task: Task):
@@ -50,13 +56,18 @@ def update_task(task_id: int, task: Task):
         if task_id in tasks_dict.keys():
             tasks_dict[task_id] = [task.name, task.status, task.description]
             file_worker.write(tasks_dict)
-            return {"Task name": task.name, "Status": task.status, "Description": task.description}
+            return {
+                "Task name": task.name,
+                "Status": task.status,
+                "Description": task.description,
+            }
         else:
             raise KeyError
     except KeyError:
         return {"KeyError": "task_id not found"}
     except:
         return {"Error": "Updating went wrong"}
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
